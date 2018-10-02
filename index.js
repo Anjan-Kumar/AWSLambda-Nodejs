c65b2e3999f95fe4130d1c7c317ae602bf20dcea
Kumar <kumar.devops0250@gmail.com>
2018-10-02 09:53:54 -0400

    Rechecking the pipeline

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = function(event, context, callback) {
     console.log("value1 = " + event.key1);
     console.log("value2 = " + event.key2);  
-    callback(null, "Final CICD of Lambda after Deployment.");
+    callback(null, "Final CICD pipeline of AWS Lambda after Deployment .");
     // or 
     // callback("some error type"); 
  }
